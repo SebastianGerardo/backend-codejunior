feb7de736b17417f33e9c487383ac66cc3fc069d
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-05-04 03:45:41 -0500

    add service redes desarrollador

--- src/services/redes.desarrollador.service.js
+++ src/services/redes.desarrollador.service.js
@@ -11,3 +11,32 @@ async function createRedesDesarrollador(data) {
     });
     return redesDesarrolladorCreada;
 }
+
+async function updateRedesDesarrollador(data) {
+    const redesDesarrolladorActualizada = await prisma.tbl_redes_desarrollador.update({
+        where: {
+            id_redes_desarrollador: data.id
+        },
+        data: {
+            desarrollador_redes_url: data.url,
+            tbl_desarrollador: data.id_desarrollador,
+            tbl_redes: data.id_redes
+        }
+    });
+    return redesDesarrolladorActualizada;
+}
+
+async function deleteRedesDesarrollador(id) {
+    const redesDesarrolladorEliminada = await prisma.tbl_redes_desarrollador.delete({
+        where: {
+            id_redes_desarrollador: id
+        }
+    });
+    return redesDesarrolladorEliminada;
+}
+
+module.exports = {
+    createRedesDesarrollador,
+    updateRedesDesarrollador,
+    deleteRedesDesarrollador
+}
