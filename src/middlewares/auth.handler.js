e3b539b4987c1ea1d3c857828b942bc2d77556fe
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-05-04 04:59:13 -0500

    SG | Error token

--- src/middlewares/auth.handler.js
+++ src/middlewares/auth.handler.js
@@ -5,17 +5,17 @@ function verifyToken(req, res, next) {
     if (typeof bearerToken !== 'undefined') {
       const bearer = bearerToken.split(' ')
       const token = bearer[1]
-      console.log("estoy en verifyToken", token, process.env.jwt_secret, bearerToken)
+      console.log("estoy en verifyToken", token, process.env.JWT_SECRET, bearerToken)
       try {
-        console.log("estoydentro verifyToken", token, process.env.jwt_secret, bearerToken)
-        const decoded = jwt.verify(token, process.env.jwt_secret)
+        console.log("estoydentro verifyToken", token, process.env.JWT_SECRET, bearerToken)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const userId = decoded.id;
         const tipoId = decoded.tipo;
         req.userId = userId; // agregamos la propiedad userId al objeto req
         req.tipoId = tipoId;
         return next()
       } catch (err) {
-        console.log("estoyfuera verifyToken", token, err, bearerToken)
+        console.log("estoydentro verifyToken", token, process.env.JWT_SECRET, bearerToken)
         return res.status(401).json({
           status: false,
           content: "errorcito"
